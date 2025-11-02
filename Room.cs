aa5d10c32586fc1f97d688be1b6bf78979e6fb79
hkmp1303 <hkmp1303@gmail.com>
2025-11-02 20:30:02 +0100

    Created Room class, constructor and status enum, save and load methods
    Required for bookings, required for saving room status

---
+++ Room.cs
@@ -0,0 +1,50 @@
+namespace App;
+
+class Room
+{
+    public int RoomNumber;
+    public RoomStatus Status;
+
+    // room constructor
+    Room(int roomNumber, RoomStatus status)
+    {
+        RoomNumber = roomNumber;
+        Status = status;
+    }
+
+    // dictionary of room numbers
+    public static Dictionary<int, Room> RoomList = new();
+
+    // load method
+    public static void LoadFromFile(string filename)
+    {
+        // read data from file into string array lines
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)              // loop through array lines
+        {
+            string[] parts = line.Split('¤');       // separate data for saving
+            if (parts.Length < 2) continue;         // Skip invalid lines
+            int roomnumber = int.Parse(parts[0]);   // initialize variable, cast data from string to int for loading
+            // add roomnumber to dictionary
+            RoomList.Add(roomnumber, new Room(roomnumber, (RoomStatus)Enum.Parse(typeof(RoomStatus), parts[1])));
+        }
+    }
+
+    // save room number to file
+    public static void SaveToFile(string filename)
+    {
+        string[] lines = new string[RoomList.Count];  // array of room numbers
+        int i = 0; // initalize variable i
+        foreach (var room in RoomList) // loop through RoomList dictionary
+        {
+            lines[i++] = $"{room.Key}¤{room.Value.Status}"; // prepare data for saving, separate by ¤
+        }
+        File.WriteAllLines(filename, lines); // write data to save file
+    }
+}
+enum RoomStatus
+{
+    unavailible,
+    reserved,
+    vacant
+}
