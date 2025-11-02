e55b516f37ae489a74fb5de263d9936a609e0b84
hkmp1303 <hkmp1303@gmail.com>
2025-11-02 17:18:56 +0100

    Added static LoadFromFile method, csv file, add Receptionist constructor
    loading user data from save file, calling constructor when adding users
    to save file

--- Receptionsist.cs
+++ Receptionsist.cs
@@ -9,5 +9,26 @@ class Receptionsist
         return Username == username && _password == password;
     }
 
+    // constructor
+    Receptionsist(string username, string password)
+    {
+        Username = username;
+        _password = password;
+    }
+
+    // dictionary of receptionsits/users
     public static Dictionary<string, Receptionsist> ReceptionistList = new();
+
+    //load method
+    static public void LoadFromFile(string filename)
+    {
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('¤');
+            if (parts.Length < 2) continue; // skip invalid lines
+            // Add receptionist to dictionary from file
+            ReceptionistList.Add(parts[0], new Receptionsist(parts[0], parts[1]));
+        }
+    }
 }
